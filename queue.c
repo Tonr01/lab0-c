aa4f5cf325dba8e598f949b825fccc583eec517d
Tonr01 <diamond871105@gmail.com>
2023-02-22 18:56:13 +0800

    Update function q_remove_head and q_remove_tail

--- queue.c
+++ queue.c
@@ -89,13 +89,35 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+
+    element_t *target = list_first_entry(head, element_t, list);
+    list_del_init(&target->list);
+
+    if (sp) {
+        strncpy(sp, target->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return target;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+
+    element_t *target = list_last_entry(head, element_t, list);
+    list_del_init(&target->list);
+
+    if (sp) {
+        strncpy(sp, target->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return target;
 }
 
 /* Return number of elements in queue */
