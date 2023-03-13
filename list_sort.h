708c406203c6004eba70f6ba8c787ea40ea5f33d
Tonr01 <diamond871105@gmail.com>
2023-03-14 00:02:48 +0800

    Import function list_sort

---
+++ list_sort.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_LIST_SORT_H
+#define _LINUX_LIST_SORT_H
+
+#include <stdint.h>
+#include "list.h"
+
+struct list_head;
+
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+typedef int
+    __attribute__((nonnull(2, 3))) (*list_cmp_func_t)(void *,
+                                                      const struct list_head *,
+                                                      const struct list_head *);
+
+__attribute__((nonnull(2, 3))) void list_sort(void *priv,
+                                              struct list_head *head,
+                                              list_cmp_func_t cmp);
+#endif
